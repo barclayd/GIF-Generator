81fd0aca00f97bdb4b661b5236e2eff8cf4e49a1
Daniel Barclay <barclaysd@me.com>
2020-12-09 11:47:25 +0000

    Setup snowpack with react typescript boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ interface AppProps {}
 function App({}: AppProps) {
   // Create the count state.
   const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+  // Create the counter (+1 every second)
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
